51c32588ace1a1bf831857f5929d86701a4401ca
mruehrmair <markus.ruehrmair@gmail.com>
2026-03-08 13:40:07 +0100

    updated disc image creation

--- OxygenSweep/created64.sh
+++ OxygenSweep/created64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-c1541 -format "c64arcadegame,1" d64 ./d64/c64arcadegame.d64
+c1541 -format "oxygen-sweep,1" d64 ./d64/oxygen-sweep.d64
 for filename in ./bin/*.prg; do
     SOURCE=$(echo $filename)
     echo $SOURCE
     DEST=$(echo ${SOURCE##*/})
     echo $DEST
-    c1541 ./d64/c64arcadegame.d64 -write $filename $DEST
+    c1541 ./d64/oxygen-sweep.d64 -write $filename $DEST
 done
 
